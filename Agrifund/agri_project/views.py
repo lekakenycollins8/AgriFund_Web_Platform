781d0289b23c13407022ee02ffa7131132fb0895
lekakenycollins8 <lekakenycollins8@gmail.com>
2024-05-20 21:08:56 +0300

    created my project landing page

--- Agrifund/agri_project/views.py
+++ Agrifund/agri_project/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def landing_page(request):
+    """renders the landing page"""
+    return render(request, "agri_project/landing_page.html")
 # Create your views here.
